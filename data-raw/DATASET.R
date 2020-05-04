d9ebef18e575649ec1fc1531a53ee5695facd7b8
Kieran Healy <kjhealy@gmail.com>
2020-05-04 17:35:17 -0400

    daily update

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -434,8 +434,8 @@ nssp_covid_er_nat <- cdccovidview::nssp_er_visits_national()
 nssp_covid_er_reg <- cdccovidview::nssp_er_visits_regional()
 
 ## Apple Mobility Data
-apple_mobility <- get_apple_data(url = "https://covid19-static.cdn-apple.com/covid19-mobility-data/2007HotfixDev43/v2/en-us",
-                                 date = "2020-04-29") %>%
+apple_mobility <- get_apple_data(url = "https://covid19-static.cdn-apple.com/covid19-mobility-data/2007HotfixDev47/v2/en-us",
+                                 date = "2020-05-03") %>%
   pivot_longer(x2020_01_13:x2020_04_29, names_to = "date", values_to = "index") %>%
   mutate(
     date = stringr::str_remove(date, "x"),
