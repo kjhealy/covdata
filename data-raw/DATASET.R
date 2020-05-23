f3f8c0d768aa3ead6905897e4385e8f8da9666ca
Kieran Healy <kjhealy@gmail.com>
2020-05-23 11:56:01 -0400

    Fix Apple daa

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -524,7 +524,7 @@ nssp_covid_er_reg <- cdccovidview::nssp_er_visits_regional()
 ## Apple Mobility Data
 apple_mobility <- get_apple_data(url = "https://covid19-static.cdn-apple.com/covid19-mobility-data/2008HotfixDev38/v3/en-us",
                                  date = "2020-05-21") %>%
-  pivot_longer(x2020_01_13:x2020_05_18, names_to = "date", values_to = "index") %>%
+  pivot_longer(x2020_01_13:x2020_05_21, names_to = "date", values_to = "index") %>%
   mutate(
     date = stringr::str_remove(date, "x"),
     date = stringr::str_replace_all(date, "_", "-"),
